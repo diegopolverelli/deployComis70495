dc787439d859fa3dfdce36067369691647e32945
Diego Polverelli <diegopolverelli@hotmail.com>
2025-07-05 14:09:17 -0300

    index - logger added

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,6 +2,9 @@ import bcrypt from 'bcrypt';
 import {fileURLToPath} from 'url';
 import { dirname } from 'path';
 
+
+import winston from "winston"
+
 export const createHash = async(password) =>{
     const salts = await bcrypt.genSalt(10);
     return bcrypt.hash(password,salts);
@@ -13,3 +16,35 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default __dirname;
+
+export const logger2=winston.createLogger(
+    {
+        levels:{
+            "grave":0, 
+            "medio":1, 
+            "leve": 2, 
+            "info": 3
+        },
+        transports:[
+            new winston.transports.Console(
+                {
+                    level: "leve", 
+                    format: winston.format.combine(
+                        winston.format.timestamp(),
+                        winston.format.colorize(
+                            {
+                                colors:{
+                                    "grave":"red", 
+                                    "medio":"yellow",
+                                    "leve":"green",
+                                    "info":"blue"
+                                }
+                            }
+                        ),
+                        winston.format.simple()
+                    )   
+                }
+            )
+        ]
+    }
+)
